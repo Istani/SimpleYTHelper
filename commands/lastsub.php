30ab97fccce9404f46b31aa83e6d7882e25958b5
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-08-29 18:16:35 +0200

    TEST für CHANGE

--- commands/lastsub.php
+++ commands/lastsub.php
@@ -3,19 +3,14 @@
 	
 	// incs
 	require 'inc/php_inc.php';
+	require 'inc/google_connect.php';
 	
 	// default
 	$subcount=0;
 	$subname="Nobody";
 	
-	 $client = new Google_Client();
-	 $client->setDeveloperKey($DEV_KEY);
-	 //$client->setClientId($OAUTH2_CLIENT_ID);
-	 //$client->setClientSecret($OAUTH2_CLIENT_SECRET);
-	 $client->setScopes('https://www.googleapis.com/auth/youtube');
-	 $youtube = new Google_Service_YouTube($client);
-	$listResponse = $youtube->channels->listChannels('statistics', array('id' =>$KANALID ,));
-	print($listResponse);
+	$listResponse = $youtube->channels->listChannels('statistics', array('id' => $KANALID));
+	$subcount=$listResponse[0]["modelData"]["statistics"]["subscriberCount"];
 	 
 	 echo "#".$subcount." - ".$subname;
 ?>
