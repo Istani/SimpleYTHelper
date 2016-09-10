852cf7baa9d23db0bad168fc77bb1200dd0540a1
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-09-10 11:40:09 +0200

    ADD random sub & subs of the day

---
+++ commands/randomsub.php
@@ -0,0 +1,20 @@
+<?
+// Getting Random SUB Name
+	
+	// incs
+	require 'inc/php_inc.php';
+	
+
+if(!file_exists("data/".$KANALID.".subs")){
+	$tmp=fopen("data/".$KANALID.".subs","w+");
+	fclose($tmp);
+}
+$file="data/".$KANALID.".subs";
+$myfile = fopen($file,"r+") or die("Unable to load SubData!");
+		$file_subs = fread($myfile,filesize($file));
+		fclose($myfile);
+		$arr_subs=explode( "|#*#|", $file_subs);
+
+	shuffle($arr_subs);
+	echo $arr_subs[0];
+?>
