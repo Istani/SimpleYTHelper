e766524d1871b333755727eebd0cbd234d4aac9c
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-10-05 20:31:19 +0200

    BOT Url Parameter

--- bot/command_scripts/defender.js
+++ bot/command_scripts/defender.js
@@ -4,10 +4,15 @@ var url = "http://31.172.95.10/SimpleYTH/do.php?command=";
 var self = module.exports = {
   execute: function (msg) {
     var parameter = msg.content.split(" ")[1];
+    var parameter2="";
+    var tmp_para=msg.content.split(" ");
+    for (var i = 2; i<tmp_para.length();i++) {
+    	parameter2=parameter2+tmp_para[i];
+    }
     if (typeof parameter == 'undefined') {
       parameter=null;
     }
-    var tmp_url = url + parameter;
+    var tmp_url = url + parameter +"&param="+parameter2;
     fetch(tmp_url)
     .then(function (response) {
       return response.text();
