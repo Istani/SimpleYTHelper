cb7b13ed020b17a2ec008c2edd5328160f300305
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-10-14 22:38:28 +0200

    START LIVESTREAM CHAT

--- commands/test.php
+++ commands/test.php
@@ -3,24 +3,46 @@
 // Getting SUB count & last name
 require 'inc/php_inc.php';
 
+$accessToken = load_accesstoken($KANALID);
+
 $database=new db("sqlite","");
 $database->connect_db("data/".$KANALID.".sqlite3");
 
-// default
-$vidtitle = 0;
-$vidlink = "Nobody";
+// Google Verbindung
+$client = new Google_Client();
+$client->setClientId($OAUTH2_CLIENT_ID);
+$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+$client->setDeveloperKey($DEV_KEY);
+$client->setScopes('https: //www.googleapis.com/auth/youtube.readonly');
 
-$check_table=$database->show_tables();
-
-$_tmp_tabellename="videos_liveStreamingDetails";
-if(in_array($_tmp_tabellename, $check_table)) {
-  $db_stats = $database->sql_select($_tmp_tabellename, "*", "videoID NOT NULL ORDER BY actualStartTime DESC", true);
-  $vidtitle="";
-  $vidlink="";
-  for ($i=0;$i<1;$i++){
-  echo	var_dump($db_stats[$i]);
-  }
+$client->setAccessToken($accessToken);
+if ($client->isAccessTokenExpired()) {
+  $client->refreshToken(load_refreshtoken($KANALID));
+  save_accesstoken($KANALID, $client->getAccessToken());
 }
 
-echo $vidtitle . "\n\r" . $vidlink;
+$youtube = new Google_Service_YouTube($client);
+
+
+//$broadcastsResponse = $youtube->channels->listChannels('contentDetails,id,localizations,snippet,statistics,status,topicDetails',array('mine' => 'true'));
+$broadcastsResponse = $youtube-> search->listSearch('id', array('channelId'=>$KANALID, 'eventType'=>'live','type'=>'video'));
+
+if (isset($broadcastsResponse["items"][0])) {
+$BroadcastId=$broadcastsResponse["items"][0]["id"]["videoId"];
+
+echo "BroadcastId:".$BroadcastId."\n\r";
+
+$broadcastsResponse = $youtube->liveBroadcasts->listLiveBroadcasts('snippet',array('id'=>$BroadcastId));
+
+$ChatId=$broadcastsResponse["items"][0]["snippet"]["liveChatId"];
+
+echo "ChatId:".$ChatId."\n\r";
+
+$broadcastsResponse = $youtube->liveChatMessages->list('snippet,authorDetails',array("liveChatId"=>$ChatId,"maxResults"=>2000));
+
+
+debug_log($broadcastsResponse);
+} else {
+	echo "Kein Livestream gefunden!";
+}
 ?>
