10ea73a452ff1d2329561e799b5c55d86f650afc
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-10-19 17:41:22 +0200

    .

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  echo "DO";
-  include("functions/func_token.php");
-  $KANALID="UC5DOhI70dI3PnLPMkUsosgw";
-  $token=load_accesstoken($KANALID);
-  $token=json_encode($token);
-  save_accesstoken($KANALID."2",$token);
-  echo "DONE";
-?>
