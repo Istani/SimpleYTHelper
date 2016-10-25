50d935c223cd9b5d648e36c74d04b278692f2b30
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-10-25 15:45:08 +0200

    Try Repeat Party

--- bot/command_scripts/party.js
+++ bot/command_scripts/party.js
@@ -1,5 +1,23 @@
 var self = module.exports = {
   execute: function (msg) {
-    msg.channel.sendTTSMessage("PARTY @everyone");
+    var amount=0;
+    var tmp_para=msg.content.split(" ");
+    if (typeof para[1]==undefiend) {
+      amount=1;
+    } else {
+      if (parseInt(para[1]>0) {
+        amount=parseInt(para[1]);
+      }
+    }
+    self.execute_repeat(msg, amount);
+    
+    
   },
+  execute_repeat:function (msg, amount) {
+    msg.channel.sendTTSMessage("PARTY @everyone");
+    amount=amount-1;
+    if (amount>0) {
+      setTimeout(self.execute_repeat(msg, amount), 1000);
+    }
+  };
 };
