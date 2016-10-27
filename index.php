75f254482e14fbcd87fa19753aec0801fa71a955
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-10-27 14:15:39 +0200

    Banner Hinzugefügt

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require_once 'inc/google_connect.php';
         <div id="helper_nav" class="ui-accordion ui-widget ui-helper-reset" width="100%">
           <h3 class="accordion-header ui-accordion-header ui-helper-reset ui-state-default ui-accordion-icons ui-corner-all">Simple YoutubeHelper</h3>
           <div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom">
-            BANNER
+            <img src="img/_Banner.png">
           </div>
         </div>
       </td>
@@ -25,7 +25,7 @@ require_once 'inc/google_connect.php';
       require_once 'site/nav_main.php';
       require_once 'site/nav_helper.php';
       ?></td>
-      <td><?php include("site/GetSubs.php"); ?></td>
+      <td><?php include("site/info.php"); ?></td>
     </tr>
   </table>
 </body>
