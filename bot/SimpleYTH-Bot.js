4f7f56f6bc36453c6c8cd9f8fae260d69c296614
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-12-20 23:17:47 +0100

    Vergessen Test zu aktivieren

--- bot/SimpleYTH-Bot.js
+++ bot/SimpleYTH-Bot.js
@@ -70,7 +70,7 @@ function Google_CheckMessage() {
   });
   setTimeout(Google_CheckMessage,100);
 }
-//Google_CheckMessage();
+Google_CheckMessage();
 // Irgendwas muss ich mir noch ausdenken, so kann ich die Datei nicht lesen
 
 
