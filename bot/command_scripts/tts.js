9976cc0090c2099059528587241dee1a0404245c
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2016-12-21 09:19:59 +0100

    User sagt

--- bot/command_scripts/tts.js
+++ bot/command_scripts/tts.js
@@ -3,8 +3,10 @@ var self = module.exports = {
     var parameter2="";
     var tmp_para=msg.content.split(" ");
     for (var i = 1; i<tmp_para.length;i++) {
-      parameter2=parameter2+tmp_para[i];
+      parameter2=parameter2+ " " +tmp_para[i];
     }
+    parameter2 = parameter2.trim();
+    parameter2 = msg.user.name + " schreibt: "+parameter2;
     msg.channel.sendTTSMessage(parameter2);
     msg.delete();
   }
