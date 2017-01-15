79999f8af5ad05537c674414ad7c4131a59ca762
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-01-15 11:13:41 +0100

    falls kein Video vorhanden ist

--- commands/lastvideo.php
+++ commands/lastvideo.php
@@ -9,9 +9,11 @@ $check_table=$database->show_tables();
 
 $_tmp_tabellename="videos_status";
 if(in_array($_tmp_tabellename, $check_table)) {
-  $db_stats = $database->sql_select($_tmp_tabellename." INNER JOIN videos_snippet ON videos_snippet.videoID=videos_status.videoId", "videos_snippet.*", "videos_snippet.ignore='0' AND videos_status.privacystatus ='public' ORDER BY videos_snippet.publishedat DESC LIMIT 1", true);
-  $vidtitle=$db_stats[0]["title"];
-  $vidlink="https://www.youtube.com/watch?v=".$db_stats[0]["videoid"];
+  $db_stats = $database->sql_select($_tmp_tabellename." INNER JOIN videos_snippet ON videos_snippet.videoID=videos_status.videoId", "videos_snippet.*", "videos_snippet.ignore='0' AND videos_status.privacystatus ='public' ORDER BY videos_snippet.publishedat DESC LIMIT 1", false);
+  if (isset($db_stats[0])) {
+    $vidtitle=$db_stats[0]["title"];
+    $vidlink="https://www.youtube.com/watch?v=".$db_stats[0]["videoid"];
+  }
 }
 
 echo $vidtitle . "\n\r" . $vidlink;
