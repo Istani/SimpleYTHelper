2cda18362be8db1b724fff9de908ff22c7e2ff99
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-01-30 11:51:18 +0100

    Bot-Command: bug_report

---
+++ bot/command_scripts/bug_report.js
@@ -0,0 +1,6 @@
+//https://github.com/Istani/SimpleYTHelper/issues
+var self = module.exports = {
+  execute: function (msg) {
+    msg.channel.sendMessage("https://github.com/Istani/SimpleYTHelper/issues").catch(console.error);
+  },
+};
