0e915fd8dbe82685c7434dd8a60709864c294fb9
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-02-07 17:34:29 +0100

    Discrod Login

--- inc/discord_connect.php
+++ inc/discord_connect.php
@@ -1,10 +1,22 @@
 <?php
-$Discord_CLIENT_ID     = '225365144497029125';
-$Discord_CLIENT_SECRET = 'b56TWYePh_P664VWKi2Liz0NQuQciozv';
-
 $redirect = filter_var('http://127.0.0.1/SimpleYTH/index.php?site=Discord_Auth', FILTER_SANITIZE_URL);
-const AUTHORIZATION_ENDPOINT = 'https://graph.facebook.com/oauth/authorize';
-const TOKEN_ENDPOINT         = 'https://graph.facebook.com/oauth/access_token';
+const AUTHORIZATION_ENDPOINT = 'https://discordapp.com/api/oauth2/authorize';
+const TOKEN_ENDPOINT         = 'https://discordapp.com/api/oauth2/token';
 
-
+$client = new OAuth2\Client($Discord_CLIENT_ID, $Discord_CLIENT_SECRET);
+if (!isset($_GET['code']))
+{
+  $auth_url = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, $redirect, array("scope"=>"identify"));
+  header('Location: ' . $auth_url);
+  die('Redirect');
+}
+else
+{
+  $params = array('code' => $_GET['code'], 'redirect_uri' => $redirect);
+  $response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
+  parse_str($response['result'], $info);
+  $client->setAccessToken($info['access_token']);
+  $response = $client->fetch('https://discordapp.com/api/oauth2/applications/@me');
+  var_dump($response, $response['result']);
+}
 ?>
--- site/my_accounts.php
+++ site/my_accounts.php
@@ -1,3 +1,6 @@
 <?php
 echo "My Accounts";
 ?>
+
+<a href="index.php?site=New_Google_Auth">Google</a><br>
+<a href="index.php?site=New_Discord_Auth">Discord</a><br>
