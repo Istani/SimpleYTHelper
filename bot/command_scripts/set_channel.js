d12dbeb6565ea37d255624ad3b36098bfe13d847
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-02-27 15:10:26 +0100

    Typo

--- bot/command_scripts/set_channel.js
+++ bot/command_scripts/set_channel.js
@@ -6,7 +6,7 @@ var self = module.exports = {
     var parts=message_row.message.split(" ");
     
     var channel="";
-    if (typof parts[2]!=="undefiend") {
+    if (typeof parts[2]!=="undefiend") {
       channel=parts[2].replace("<#","");
       channel=channel.replace(">","");
     }
