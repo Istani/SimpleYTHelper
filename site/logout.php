e4c44b08e788c69b3d86e58207e9192721866b46
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-03-12 15:59:10 +0100

    Logout Link richtig

--- site/logout.php
+++ site/logout.php
@@ -2,7 +2,7 @@
 @session_start();
 unset($_SESSION);
 @session_destroy();
-header("Location: http://127.0.0.1/SimpleYTH/");
+header("Location: http://simpleyth.randompeople.de");
 //header("Location: http://simpleyth.randompeople.de");
 echo '<a href="http://simpleyth.randompeople.de">Zurück zur Seite</a>';
 ?>
