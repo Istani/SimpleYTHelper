f5d1343a074843b2192f7c4b37378a79029f268d
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-03-12 18:56:46 +0100

    More Errors

--- do.php
+++ do.php
@@ -29,7 +29,7 @@ if (isset($_GET['host'])) {
     }
   }
 }
-$_SESSION['user']=$user;
+$_SESSION['user']=$user[0];
 
 if (!isset($_GET["command"]) OR $_GET["command"] == "" OR $_GET["command"] == "null") {
   echo "No Command given!
