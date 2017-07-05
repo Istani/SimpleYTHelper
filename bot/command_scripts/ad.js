4c681e9a4a004fa46718ad9c8069c1f38f191b8b
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-07-05 12:28:06 +0200

    vergessen zu speichern

--- bot/command_scripts/ad.js
+++ bot/command_scripts/ad.js
@@ -26,14 +26,30 @@ var self = module.exports = {
         return;
       }
       for (var i = 0; i<rows.length;i++) {
-        var SQL2 = "SELECT * FROM user_ads WHERE count='"++"' ORDER BY Rand() LIMIT 1";
+        var SQL2 = "SELECT * FROM user_ads WHERE count='"+rows[i].count+"' ORDER BY Rand() LIMIT 1";
         mysql.query(SQL2, function (err2, rows2) {
-          if (err != null) {
+          if (err2 != null) {
             console.log(SQL2);
-            console.log(err);
+            console.log(err2);
             return;
           }
-          
+          for (var j = 0; j<rows2.length;j++) {
+            var SQL3 = "UPDATE user_ads SET count=count+1 WHERE link='"+rows2[j].link+"'";
+            mysql.query(SQL3, function (err3, rows3) {
+              if (err3 != null) {
+                console.log(SQL3);
+                console.log(err3);
+                return;
+              }
+            });
+            var ReturnString="(AD) ";
+            var RowString="";
+            RowString=rows2[j].title;
+            RowString+=": ";
+            RowString+=rows2[j].link;
+            RowString+="\r\n";
+            ReturnString+=RowString;
+          }
         });
       }
       
