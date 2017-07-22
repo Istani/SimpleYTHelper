37244fbf9d1df07f0851d4dccd7d7448f5258ea2
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-07-22 22:32:38 +0200

    Versuch Own Ad

--- bot/command_scripts/ad.js
+++ bot/command_scripts/ad.js
@@ -18,51 +18,67 @@ var self = module.exports = {
     }
   },
   execute: function (message_row, SendFunc, NewMessageFunc) {
-    var My_Owner="admin";
-    var SQL = "SELECT * FROM user_ads WHERE owner='"+My_Owner+"' AND type NOT LIKE 'Link' AND link NOT LIKE '' ORDER BY count LIMIT 1";
-    mysql.query(SQL, function (err, rows) {
-      if (err != null) {
-        console.log(SQL);
-        console.log(err);
-        return;
+    var My_Owner="";
+    var GET_OWNER="SELECT * FROM bot_chathosts WHERE host='"+message_row.host+"'";
+    mysql.query(GET_OWNER, function (oerr, orows) {
+      if (orows.length==0) {
+        orows[0].owner="202892723420659714";
       }
-      for (var i = 0; i<rows.length;i++) {
-        var SQL2 = "SELECT * FROM user_ads WHERE count='"+rows[i].count+"' AND type NOT LIKE 'Link' AND link NOT LIKE '' AND owner='"+My_Owner+"' ORDER BY Rand() LIMIT 1";
-        mysql.query(SQL2, function (err2, rows2) {
-          if (err2 != null) {
-            console.log(SQL2);
-            console.log(err2);
-            return;
+      for (var num_orows = 0; num_orows<orows.length;num_orows++) {
+        var GET_ACCOUNT="SELECT * FROM user WHERE discord_user='"+orows[num_orows].owner+"' or youtube_user='"+orows[num_orows].owner+"'";
+        mysql.query(GET_ACCOUNT, function (aerr, arows) {
+          if (arows.length==0) {
+            arows[0].email="simpleyth";
           }
-          for (var j = 0; j<rows2.length;j++) {
-            if (rows2[j].count>=2) {
-              var SQL3 = "UPDATE user_ads SET count=0 WHERE owner='"+rows2[j].owner+"'";
-              mysql.query(SQL3, function (err3, rows3) {
-                if (err3 != null) {
-                  console.log(SQL3);
-                  console.log(err3);
-                  return;
-                }
-              });
-            }
-            var SQL4 = "UPDATE user_ads SET count=count+1 WHERE link='"+rows2[j].link+"'";
-            mysql.query(SQL4, function (err4, rows4) {
-              if (err4 != null) {
-                console.log(SQL4);
-                console.log(err4);
+          for (var num_arows = 0; num_arows<arows.length;num_arows++) {
+            My_Owner=arows[num_arows].email;
+            var SQL = "SELECT * FROM user_ads WHERE owner='"+My_Owner+"' AND type NOT LIKE 'Link' AND link NOT LIKE '' ORDER BY count LIMIT 1";
+            mysql.query(SQL, function (err, rows) {
+              if (err != null) {
+                console.log(SQL);
+                console.log(err);
                 return;
               }
+              for (var i = 0; i<rows.length;i++) {
+                var SQL2 = "SELECT * FROM user_ads WHERE count='"+rows[i].count+"' AND type NOT LIKE 'Link' AND link NOT LIKE '' AND owner='"+My_Owner+"' ORDER BY Rand() LIMIT 1";
+                mysql.query(SQL2, function (err2, rows2) {
+                  if (err2 != null) {
+                    console.log(SQL2);
+                    console.log(err2);
+                    return;
+                  }
+                  for (var j = 0; j<rows2.length;j++) {
+                    if (rows2[j].count>=2) {
+                      var SQL3 = "UPDATE user_ads SET count=0 WHERE owner='"+rows2[j].owner+"'";
+                      mysql.query(SQL3, function (err3, rows3) {
+                        if (err3 != null) {
+                          console.log(SQL3);
+                          console.log(err3);
+                          return;
+                        }
+                      });
+                    }
+                    var SQL4 = "UPDATE user_ads SET count=count+1 WHERE link='"+rows2[j].link+"'";
+                    mysql.query(SQL4, function (err4, rows4) {
+                      if (err4 != null) {
+                        console.log(SQL4);
+                        console.log(err4);
+                        return;
+                      }
+                    });
+                    
+                    var ReturnString="(AD) ";
+                    var RowString="";
+                    RowString=rows2[j].title;
+                    RowString+=": ";
+                    RowString+=rows2[j].link;
+                    RowString+="\r\n";
+                    ReturnString+=RowString;
+                    SendFunc(ReturnString);
+                  }
+                });
+              }
             });
-            
-            
-            var ReturnString="(AD) ";
-            var RowString="";
-            RowString=rows2[j].title;
-            RowString+=": ";
-            RowString+=rows2[j].link;
-            RowString+="\r\n";
-            ReturnString+=RowString;
-            SendFunc(ReturnString);
           }
         });
       }
