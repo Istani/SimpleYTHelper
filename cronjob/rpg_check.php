82d033867ff97e747fe13531b6ac2d20256ebd25
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-07-25 18:37:53 +0200

    Calc AVG Test

--- cronjob/rpg_check.php
+++ cronjob/rpg_check.php
@@ -24,12 +24,46 @@ if ($tt["last_used"]+$tt["cooldown"]<time()) {
 		$database->create_table($cronjob_id, $felder, "game_id");
 		unset($felder);
 	}
-	//die("RPG CHECK");
+	
 	// Additional Later Added Cols
+	$new_feld["calculate_avg"]="INT DEFAULT 1";
+	$new_feld["factor"]="INT DEFAULT 100";
+	$database->add_columns($cronjob_id, $new_feld);
+	unset($new_feld);
 	
 	// Do Magic!
+	$game_data=$database->sql_select($cronjob_id, "*", "game_id='".md5($_SESSION['user']['email'])."'", false);
+	if (isset($game_data[0])) {
+		$this_game=$game_data[0];
+		// YEAH
+		$change_me=true;
+		switch ($this_game['game_state']) {
+			case 0:
+			$this_game['game_state']=1;
+			// Calculate Values for this RPG
+			$temp_avg=0;
+			$temp_count=0;
+			$game_data=$database->sql_select("bot_chatuser INNER JOIN bot_chathosts ON bot_chatuser.service=bot_chathosts.service AND bot_chatuser.host=bot_chathosts.host", "bot_chatuser.*", "bot_chathosts.owner='".$_SESSION['user']['youtube_user']."' or bot_chathosts.owner='".$_SESSION['user']['discord_user']."'", false);
+			for ($count_game_data=0;$count_game_data<count($game_data);$count_game_data++) {
+				$this_user=$game_data[$count_game_data];
+				$temp_avg=$this_user['msg_avg'];
+				$temp_count++;
+			}
+			$temp_avg=$temp_avg/$temp_count;
+			$this_game['calculate_avg']=$temp_avg;
+			
+			break;
+			default:
+			$change_me=false;
+		}
+		if ($change_me) {
+			$database->sql_insert_update($cronjob_id,$this_game);
+		}
+		
+		
+	}
 	
-	
+	//die("RPG CHECK");
 	// Save Token
 	echo date("d.m.Y - H:i:s")." - ".$_SESSION['user']['email'].': '.$cronjob_id." updated!<br>";
 	$tt["last_used"]=time();
