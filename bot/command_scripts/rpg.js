fb418e7839c357a21dfc0d53575d22404748764f
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-10-01 18:27:11 +0200

    role add on mvp

--- bot/command_scripts/rpg.js
+++ bot/command_scripts/rpg.js
@@ -74,7 +74,7 @@ var self = module.exports = {
           }
           case 'result':
           if ((message_row.user=="-1")) {
-            CheckResult(SendFunc,HOST_USER.hash);
+            CheckResult(SendFunc,HOST_USER.hash, NewMessageFunc, message_row);
             break;
           }
           break;
@@ -245,7 +245,7 @@ function CheckRound(SendFunc, GameID) {
     }
   });
 }
-function CheckResult(SendFunc,GameID) {
+function CheckResult(SendFunc,GameID, NewMessageFunc, old_message) {
   var CHECK_STATE="SELECT * FROM rpg_check WHERE game_id='"+GameID+"'";
   mysql.query(CHECK_STATE, function (err, check_state_rows) {
     if (err != null) {
@@ -270,6 +270,13 @@ function CheckResult(SendFunc,GameID) {
           ReturnString="";
         }
         ReturnString+=RowString;
+        
+        if ((i==0) && (list_player_dmg[i].user_name!="")) {
+          old_message.content="!set_role "+list_player_dmg[i].user_name;
+          old_message.id=old_message.id*10;
+          old_message.user="-1";
+          NewMessageFunc(old_message.service, old_message.host, old_message.room, old_message.id, old_message.time, old_message.user, old_message.content);
+        }
       }
       if (ReturnString.length>7) {
         SendFunc(ReturnString);
