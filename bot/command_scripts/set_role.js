b65a37da90c2726440e0fcce7731183e8468942d
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-10-01 17:52:36 +0200

    debug console

--- bot/command_scripts/set_role.js
+++ bot/command_scripts/set_role.js
@@ -60,6 +60,7 @@ var self = module.exports = {
     var this_member=null;
     for (var [key, member] of all_member) {
       var member_name=member.name;
+      console.log(member.name);
       if (typeof member_name == "undefined") {
         member_name="";
       }
