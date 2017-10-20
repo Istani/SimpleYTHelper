7243f6866a5eb022df76c6cd903dab62d9dfaa5b
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-10-20 15:18:04 +0200

    More Sponsors

--- commands/sponsors.php
+++ commands/sponsors.php
@@ -9,7 +9,7 @@ $check_table=$database->show_tables();
 
 $_tmp_tabellename=strtolower("youtube_sponsors");
 if(in_array($_tmp_tabellename, $check_table)) {
-  $db_subs = $database->sql_select($_tmp_tabellename, "*", "sponsorid='".$_SESSION['user']['youtube_user']."'ORDER BY first_seen DESC LIMIT 1",true);
+  $db_subs = $database->sql_select($_tmp_tabellename, "*", "sponsorid='".$_SESSION['user']['youtube_user']."'ORDER BY first_seen",true);
   for ($cnt_sponsors=0;$cnt_sponsors<count($db_subs);$cnt_sponsors++) {
     echo $db_subs[$cnt_sponsors]["displayname"]."\r\n";
   }
