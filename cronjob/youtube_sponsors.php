9f0d0d1c9c72f436189cd1d5f5ff5cf3cf5a9e6f
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-10-20 15:19:22 +0200

    Sponsors nur einmal je Stunde Checken

--- cronjob/youtube_sponsors.php
+++ cronjob/youtube_sponsors.php
@@ -43,7 +43,7 @@ if ($tt["last_used"]+$tt["cooldown"]<time()) {
     $database->sql_insert_update($_tmp_tabellename, $this_sponsor);
     unset($this_sponsor);
   }
-  $tt["cooldown"]=60;
+  $tt["cooldown"]=60*60;
 }
 // Save Token
 echo date("d.m.Y - H:i:s")." - ".$tmp_token['channel_id'].': '.$_tmp_tabellename." updated!<br>";
