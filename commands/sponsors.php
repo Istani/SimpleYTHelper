316153f7faf91ff693361df4bc9b5b0d666c89fb
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-10-20 15:16:51 +0200

    !yt sponsors

---
+++ commands/sponsors.php
@@ -0,0 +1,21 @@
+<?php
+require 'inc/php_inc.php';
+
+// default
+$subcount = 0;
+$subname = "Nobody";
+
+$check_table=$database->show_tables();
+
+$_tmp_tabellename=strtolower("youtube_sponsors");
+if(in_array($_tmp_tabellename, $check_table)) {
+  $db_subs = $database->sql_select($_tmp_tabellename, "*", "sponsorid='".$_SESSION['user']['youtube_user']."'ORDER BY first_seen DESC LIMIT 1",true);
+  for ($cnt_sponsors=0;$cnt_sponsors<count($db_subs);$cnt_sponsors++) {
+    echo $db_subs[$cnt_sponsors]["displayname"]."\r\n";
+  }
+  
+} else {
+  echo "Kein VIP gefunden!";
+}
+
+?>
