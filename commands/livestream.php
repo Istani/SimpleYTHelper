232dd9b48b960467ca6904f46c48c95a83d57599
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2017-12-18 16:20:41 +0100

    aaah

--- commands/livestream.php
+++ commands/livestream.php
@@ -5,7 +5,7 @@ require 'inc/php_inc.php';
 $_tmp_tabellename=strtolower("youtube_livestream");
 $videos_yt=$database->sql_select("youtube_livestream", "*", "youtube_snippet_channelid='".$_SESSION['user']['youtube_user']."' AND youtube_snippet_actualendtime IS NULL ORDER BY youtube_snippet_actualstarttime DESC LIMIT 1",false);
 
-if (count($videos_yt)>=0) {
+if (count($videos_yt)>0) {
   echo "http://gaming.youtube.com/channel/".$_SESSION['user']['youtube_user']."/live";
 } else {
   echo "Kein Livestream gefunden!";
