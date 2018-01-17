8f0177e8260f46405b86d21765ed68700a2ec56b
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-01-17 12:27:32 +0100

    hat irgendwie nicht geklappt

--- cronjob/bot_cleanup_db.php
+++ cronjob/bot_cleanup_db.php
@@ -25,42 +25,6 @@ if ($tt["last_used"]+$tt["cooldown"]<time()) {
   
   $database->sql_delete("youtube_videos", "`simple_lastupdate`<".$oldest_date."");
   
-  // Zumindest Einmalig benötigt
-  $check_table=$database->show_tables();
-  if(!in_array("channel_token", $check_table)) {
-    // Warten mal früher benutzet Tabellen...
-    $database->sql_droptable("bot_chat_stats");
-    $database->sql_droptable("channel_token");
-    $database->sql_droptable("channels_contentdetails");
-    $database->sql_droptable("channels_livestreamchat");
-    $database->sql_droptable("channels_statistics");
-    $database->sql_droptable("ergebnisse");
-    $database->sql_droptable("livestream_chat");
-    $database->sql_droptable("playlistitems_id");
-    $database->sql_droptable("playlists_snippet");
-    $database->sql_droptable("rpg_check");
-    $database->sql_droptable("rpg_player");
-    $database->sql_droptable("rpg_player_attack");
-    $database->sql_droptable("rpg_settings");
-    $database->sql_droptable("rss_news");
-    $database->sql_droptable("rss_news_source");
-    $database->sql_droptable("simpleyth_rss_automation");
-    $database->sql_droptable("simpleyth_rss_posts");
-    $database->sql_droptable("simpleyth_rss_source");
-    $database->sql_droptable("subscriptions_subscribersnippet");
-    $database->sql_droptable("temp_test");
-    $database->sql_droptable("temp_test_tags");
-    $database->sql_droptable("umfragen");
-    $database->sql_droptable("user_voted");
-    $database->sql_droptable("videos_contentdetails");
-    $database->sql_droptable("videos_livestreamingdetails");
-    $database->sql_droptable("videos_snippet");
-    $database->sql_droptable("videos_snippet_tags");
-    $database->sql_droptable("videos_statistics");
-    $database->sql_droptable("videos_status");
-    $database->sql_droptable("youtube_video_tags");
-    unset($felder);
-  }
   $tt["cooldown"]=1*60*60;
 }
 // Save Token
