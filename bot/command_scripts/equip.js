675e7fcfe9688e54112fd5c9f312218e07c79b63
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-01-19 15:04:55 +0100

    Nicht die Neuen Ads Posten

--- bot/command_scripts/equip.js
+++ bot/command_scripts/equip.js
@@ -19,7 +19,7 @@ var self = module.exports = {
   },
   execute: function (message_row, SendFunc, NewMessageFunc) {
     var My_Owner="admin";
-    var SQL = "SELECT *, md5(concat(owner,link)) AS hash FROM user_ads WHERE type NOT LIKE 'AD' AND type NOT LIKE 'Link' AND link NOT LIKE '' AND owner='"+My_Owner+"'";
+    var SQL = "SELECT *, md5(concat(owner,link)) AS hash FROM user_ads WHERE type NOT LIKE 'AD' AND type NOT LIKE 'AD%' AND type NOT LIKE 'Link' AND link NOT LIKE '' AND owner='"+My_Owner+"'";
     mysql.query(SQL, function (err, rows) {
       if (err != null) {
         console.log(SQL);
