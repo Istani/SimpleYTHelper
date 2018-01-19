c7042f8b8d59d9cb5817a63dc688c198f42c282c
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-01-19 13:50:54 +0100

    Amazon Key gelöscht und in private_settings.php hinzugefügt

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
+require_once 'inc/php_inc.php';
 
 require_once 'API/Amazon/lib/AmazonECS.class.php';
-$client = new AmazonECS('AKIAI2IMWNO2X6FUNLOQ', '6h93VVOx39on/6tQSu/sIMq2DGmvq/kNWQNX1Im1', 'DE', 'istani0815-21');
+$client = new AmazonECS($amazon['API_KEY'], $amazon['API_Secret'], 'DE', 'istani0815-21');
 
 $response  = $client->category('Books')->search('PHP 5');
-var_dump($response);
+debug_log($response);
 ?>
