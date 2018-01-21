40b4a3dfd5cb0f5711cfa51d64586c3663903674
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-01-21 15:22:00 +0100

    BugFix: !discord - Hat bei YT immer den Bot gekillt!

--- bot/command_scripts/discord.js
+++ bot/command_scripts/discord.js
@@ -30,32 +30,32 @@ var self = module.exports = {
     var count_user=0;
     var guilds =discord.guilds;
     var msg_guild=null;
-    /*if (message_row.service!="Discord") {
-    SendFunc("Diese Funktion macht nur im Discord Sinn!");
-  }*/
-  for (var [key, guild] of guilds) {
-    count_server++;
-    if (guild.id==message_row.host) {
-      msg_guild=guild;
+    if (message_row.service!="Discord") {
+      return;
     }
-    UpdateHosts("Discord", guild.id, guild.name, guild.ownerID);
-    var members =guild.members;
-    for (var [key, member] of members) {
-      count_user++;
-      var UserRoles=[];
-      guild.member(member.user).roles.forEach(function(element){
-        UserRoles.push(element.name);
-      });
-      UpdateUser("Discord", guild.id, member.user.id, member.user.username, UserRoles);
+    for (var [key, guild] of guilds) {
+      count_server++;
+      if (guild.id==message_row.host) {
+        msg_guild=guild;
+      }
+      UpdateHosts("Discord", guild.id, guild.name, guild.ownerID);
+      var members =guild.members;
+      for (var [key, member] of members) {
+        count_user++;
+        var UserRoles=[];
+        guild.member(member.user).roles.forEach(function(element){
+          UserRoles.push(element.name);
+        });
+        UpdateUser("Discord", guild.id, member.user.id, member.user.username, UserRoles);
+      }
     }
-  }
-  SendFunc("Discord: "+count_server+" Server with "+count_user+" User");
-  if ((msg_guild.id!=null) && (message_row.user==msg_guild.ownerID || message_row.user==-1)) {
-    msg_guild.pruneMembers(30).then(pruned => SendFunc("I just pruned "+pruned+" people!")).catch(console.error);
-  } else {
-    msg_guild.pruneMembers(30).then(pruned => {}).catch(console.error);
-  }
-},
+    SendFunc("Discord: "+count_server+" Server with "+count_user+" User");
+    if ((msg_guild.id!=null) && (message_row.user==msg_guild.ownerID || message_row.user==-1)) {
+      msg_guild.pruneMembers(30).then(pruned => SendFunc("I just pruned "+pruned+" people!")).catch(console.error);
+    } else {
+      msg_guild.pruneMembers(30).then(pruned => {}).catch(console.error);
+    }
+  },
 };
 var mysql=null;
 var discord=null;
