2046b304d1130672984aaf9898262f73380fb364
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-03-22 12:04:24 +0100

    Man sollte seine eigene Klasse schon kennen!!!111einself

--- bot_php_commands/discord.php
+++ bot_php_commands/discord.php
@@ -1,7 +1,7 @@
 <?php
 $get_owner=$database->sql_select("bot_chathosts","*","host='".$this_msg['host']."'",true)[0]['owner'];
 $owner_user=$database->sql_select("user","*","discord_user='".$get_owner."' or youtube_user='".$get_owner."'",true)[0];
-$get_discord_link=$database->sql_Select("user_ads","*","WHERE type='Link' AND title LIKE 'Discord' AND link NOT LIKE '' AND owner='".$owner_user['email']."'",true)[0];
+$get_discord_link=$database->sql_Select("user_ads","*","type='Link' AND title LIKE 'Discord' AND link NOT LIKE '' AND owner='".$owner_user['email']."'",true)[0];
 if ($get_discord_link['link']!="") {
   echo $get_discord_link['title'].': '.$get_discord_link['link'];
 }
