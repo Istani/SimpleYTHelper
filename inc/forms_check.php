f7e9c54d592f689f84cc625dd946feb4f8b86d0f
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-04-11 10:32:34 +0200

    Sorry aber keine unnötigen Accounts mehr erstellen

--- inc/forms_check.php
+++ inc/forms_check.php
@@ -17,18 +17,7 @@ if (isset($_POST['login'])) {
       $_SESSION['user']=$user_rows[0];
       $_GET['site']="my_accounts";
     } else {
-      if (isset($new_data)) {
-        unset($new_data);
-      }
-      $new_data['email']=$_POST['email'];
-      $new_data['password']=$_POST['passwort'];
-      $new_data['last_login']=time();
-      $database->sql_insert_update("user", $new_data);
-      $user_rows=$database->sql_select("user", "*", "email='".$_POST['email']."' AND password='".$_POST['passwort']."'",true);
-      if ($user_rows[0]['email']==$_POST['email']) {
-        $_SESSION['user']=$user_rows[0];
-        $_GET['site']="my_accounts";
-      }
+      die("Falsche Login Informationen!");
     }
   }
 }
