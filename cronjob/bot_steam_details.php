e03acddd9aeb73a8a31e165564658c530fa07a41
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-05-23 10:14:22 +0200

    Steam Details Import Error

--- cronjob/bot_steam_details.php
+++ cronjob/bot_steam_details.php
@@ -27,49 +27,51 @@ if ($tt["last_used"]+$tt["cooldown"]<time()) {
     
     $client = new OAuth2\Client(NULL, NULL);
     $response = $client->fetch('http://store.steampowered.com/api/appdetails?appids='.$game_to_update['steam_id'].'&cc=de&l=de');
-    $response = $response["result"][$game_to_update['steam_id']];
-    if (isset($response['data'])) {
-      $response=$response['data'];
-      
-      unset($response['type']);
-      unset($response['steam_appid']);
-      unset($response['mac_requirements']);
-      unset($response['pc_requirements']);
-      unset($response['linux_requirements']);
-      unset($response['is_free']);
-      unset($response['packages']);
-      unset($response['package_groups']);
-      unset($response['categories']);
-      unset($response['screenshots']);
-      unset($response['movies']);
-      unset($response['achievements']);
-      unset($response['background']);
-
-      unset($response['dlc']);  // Ja es gibt spiele die zu viele DLC haben
-      $tmp_details=$SYTHS->multiarray2array($response, "steam");
-      
-      $tmp_details['steam_app_id']=$game_to_update['steam_id'];
-      foreach ($tmp_details as $key=>$value){
-        $new_feld[$key]="TEXT";
-        $database->add_columns($_tmp_tabellename, $new_feld);
-        unset($new_feld);
-        $newData[$key]=$value;
+    if (isset($response["result"][$game_to_update['steam_id']])) {
+      $response = $response["result"][$game_to_update['steam_id']];
+      if (isset($response['data'])) {
+        $response=$response['data'];
+        
+        unset($response['type']);
+        unset($response['steam_appid']);
+        unset($response['mac_requirements']);
+        unset($response['pc_requirements']);
+        unset($response['linux_requirements']);
+        unset($response['is_free']);
+        unset($response['packages']);
+        unset($response['package_groups']);
+        unset($response['categories']);
+        unset($response['screenshots']);
+        unset($response['movies']);
+        unset($response['achievements']);
+        unset($response['background']);
+        
+        unset($response['dlc']);  // Ja es gibt spiele die zu viele DLC haben
+        $tmp_details=$SYTHS->multiarray2array($response, "steam");
+        
+        $tmp_details['steam_app_id']=$game_to_update['steam_id'];
+        foreach ($tmp_details as $key=>$value){
+          $new_feld[$key]="TEXT";
+          $database->add_columns($_tmp_tabellename, $new_feld);
+          unset($new_feld);
+          $newData[$key]=$value;
+        }
+        debug_log($newData);
+        $database->sql_insert_update($_tmp_tabellename, $newData);
+        unset($newData);
       }
-      debug_log($newData);
-      $database->sql_insert_update($_tmp_tabellename, $newData);
-      unset($newData);
+      
+      if (isset($response["header_image"])) {$game_to_update['banner']=$response["header_image"];}
+      
+      if (isset($response["short_description"])) {$game_to_update['description']=$response["short_description"];}
+      if (isset($response["detailed_description"])) {$game_to_update['description']=$response["detailed_description"];}
+      
+      $game_to_update['steam_price']=$tmp_details['steam_price_overview_final']/100;
+      $game_to_update['last_import']=time();
+      $database->sql_insert_update("bot_gamelist",$game_to_update);
+      
+      debug_log($game_to_update);
     }
-    
-    if (isset($response["header_image"])) {$game_to_update['banner']=$response["header_image"];}
-    
-    if (isset($response["short_description"])) {$game_to_update['description']=$response["short_description"];}
-    if (isset($response["detailed_description"])) {$game_to_update['description']=$response["detailed_description"];}
-    
-    $game_to_update['steam_price']=$tmp_details['steam_price_overview_final']/100;
-    $game_to_update['last_import']=time();
-    $database->sql_insert_update("bot_gamelist",$game_to_update);
-    
-    debug_log($game_to_update);
   }
   $tt["cooldown"]=1*60;
 }
