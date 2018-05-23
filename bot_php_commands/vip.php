8315913bcc1769805e1948173688fdfc9ba5bf5d
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-05-23 14:41:10 +0200

    !vip

---
+++ bot_php_commands/vip.php
@@ -0,0 +1,5 @@
+<?php
+$get_owner=$database->sql_select("bot_chathosts","*","host='".$this_msg['host']."'",true)[0]['owner'];
+$owner_user=$database->sql_select("user","*","discord_user='".$get_owner."' or youtube_user='".$get_owner."' or twitch_user='".$get_owner."'",true)[0];
+echo "https://gaming.youtube.com/channel/".$owner_user['youtube_user']."#action=sponsor";
+?>
