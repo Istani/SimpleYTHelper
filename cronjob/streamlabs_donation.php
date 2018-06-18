c5fad686bba02abb6a183d35af6aaf2722e12ae0
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-06-18 14:28:45 +0200

    Refresh Streamlabs Token - Test

--- cronjob/streamlabs_donation.php
+++ cronjob/streamlabs_donation.php
@@ -10,13 +10,25 @@ if ($do_job==false) {
 $tt=$token[$cronjob_id];
 
 if ($tt["last_used"]+$tt["cooldown"]<time()) {
+  
+  /* REFRESH TOKEN */
+  $params=array(
+    "refresh_token"=>$tmp_token["refresh_token"]
+  );
+  $Streamlabs_TOKEN_ENDPOINT         = 'https://streamlabs.com/api/v1.0/token';
+  $response = $client->getAccessToken($Streamlabs_TOKEN_ENDPOINT, 'RefreshToken', $params);
+  $response=$response['result'];
+  $response['user']=$_SESSION['user']['email'];
+  $response['service']="Streamlabs";
+  authtoken_save($database, $response);
+  
   $params=array(
     'currency'      => 'EUR',
     'limit'         => 99
   );
   $response = $client->fetch('https://streamlabs.com/api/v1.0/donations', $params);
   debug_log($response);
-  $tt["cooldown"]=60*10;
+  $tt["cooldown"]=60*60;
 }
 // Save Token
 echo date("d.m.Y - H:i:s")." - ".$_SESSION['user']['streamlabs_user'].': '.$cronjob_id." updated!<br>";
