1a240bef972baeb634e41780898d77ebf5416d7b
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-06-23 10:40:21 +0200

    Install i18n for multi language

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var exphbs  = 
 require('express-handlebars');
+var i18n = require("i18n");
+
 
 var app = express();
 
 app.engine('handlebars', 
 exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+i18n.configure({
+    defaultLocale: 'de',
+    cookie: 'syth_language',
+    directory: './locales',
+    queryParameter: 'lang',
+    extension: '.js'
+});
+app.use(i18n.init);
+
+var hbs = exphbs.create({
+    helpers: {
+        __: function () { 
+            return i18n.__.apply(this, 
+arguments); 
+        }
+    }
+});
 
 app.get('/', function (req, res) {
     res.render('home');
