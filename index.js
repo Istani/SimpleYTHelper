4900fa01187f4ada6400b51b7ddabbb2d3735ce4
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-06-26 11:30:10 +0200

    Install DotEnv

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+/* Checking Example File for New Data! */
+var config = require('dotenv').config();
+var fs = require('fs');
+var config_example = "";
+for (var attributename in config.parsed) {
+    config_example += attributename + "=\r\n";
+}
+fs.writeFileSync('./.env.example', config_example);
+/* Example File Finish */
+
 var express = require('express');
-var exphbs  = 
-require('express-handlebars');
+var exphbs = require('express-handlebars');
 var i18n = require("i18n");
 
 
 var app = express();
 
-app.engine('handlebars', 
-exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 i18n.configure({
@@ -21,9 +29,9 @@ app.use(i18n.init);
 
 var hbs = exphbs.create({
     helpers: {
-        __: function () { 
-            return i18n.__.apply(this, 
-arguments); 
+        __: function () {
+            return i18n.__.apply(this,
+                arguments);
         }
     }
 });
@@ -32,4 +40,4 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log('Webinterface Gestartet!'));
