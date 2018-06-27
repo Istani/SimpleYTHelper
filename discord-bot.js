c659b8258a1c4fc303a9cbe5cafb272b3490336f
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-06-27 21:50:05 +0200

    Optimize Discord Login/Error

--- discord-bot.js
+++ discord-bot.js
@@ -8,15 +8,35 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('error', err => {
+  console.log("Client Error", err);
+  process.exit(1);
 });
+
+client.on('disconnect', event => {
+  console.log("Client Event", "Disconnect");
+  process.exit(1);
+});
+
 db.query("SELECT user_token FROM simpleyth_oauth_botcredentials WHERE service='discord'", function (err, result) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
-  client.login(result[0].user_token);
+  if (result.length == 0) {
+    console.log("Entry Discord-Token Missing");
+    process.exit(1);
+  }
+  client.login(result[0].user_token).catch(function (err) {
+    if (err) {
+      console.log("Client Login", err.code);
+      process.exit(1);
+    }
+  });
 });
+
+/* Custom Stuff */
+client.on('message', msg => {
+  console.log("MSG:", msg);
+});
+
