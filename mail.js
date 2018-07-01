f83875d4d7399974a63ab852ae178b4d206befc0
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-07-01 19:30:49 +0200

    Test E-Mail

--- mail.js
+++ mail.js
@@ -1,30 +1,45 @@
 'use strict';
 const nodemailer = require('nodemailer');
 
-nodemailer.createTestAccount((err, account) => {
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: '',
-            pass: ''
-        }
-    });
+const db = require('./db.js');
 
-    let mailOptions = {
-        from: '"Simple YTH" <s>',
-        to: 'sascha.u.kaufmann@googlemail.com',
-        subject: 'Hello ✔',
-        text: 'Hello world?',
-        html: '<b>Hello world?</b>'
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info);
-    });
+db.query("SELECT login, password FROM simpleyth_oauth_botcredentials WHERE service='email'", function (err, result) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    if (result.length == 0) {
+        console.log("Entry Email-Credentials Missing");
+        process.exit(1);
+    }
+    SendMail(result[0]);
 });
 
+function SendMail(email_data) {
+    nodemailer.createTestAccount((err, account) => {
+        let transporter = nodemailer.createTransport({
+            host: 'webbox14.server-home.org',
+            port: 587,
+            secure: false,
+            auth: {
+                user: email_data.login,
+                pass: email_data.password
+            }
+        });
+
+        let mailOptions = {
+            from: '"Simple YTH" <simpleyth@randompeople.de>',
+            to: 'sascha.u.kaufmann@googlemail.com',
+            subject: 'Testmail',
+            text: 'Hello world?',
+            html: '<b>Hello world?</b>'
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info);
+        });
+    });
+}
---
+++ migrations/20180701170800-BotCredentials.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+var tableName = "simpleyth_oauth_botcredentials";
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  db.addColumn(tableName, "login", { type: 'char', length: 50, notNull: true });
+  db.addColumn(tableName, "password", { type: 'char', length: 100, notNull: true });
+  return null;
+};
+
+exports.down = function (db) {
+  db.removeColumn(tableName, "login", callback);
+  db.removeColumn(tableName, "password", callback);
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
