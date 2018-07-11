3798d0ba11a640d0dc272f69c26e872ac1fbcb5e
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-07-11 22:46:37 +0200

    PREPARE Refresh Token

--- youtube_bot.js
+++ youtube_bot.js
@@ -75,6 +75,38 @@ async.parallel([
   GetChannel(OAuth_Bot_User);
 });
 
+function Refresh_Token(token_data, org_function) {
+  var request_data = {
+    "grant_type": "refresh_token",
+    "refresh_token": token_data.refresh_token,
+    "client_id": OAuth_Settings.client_id,
+    "client_secret": OAuth_Settings.client_secret
+  };
+  request({
+    url: OAuth_Settings.url_token,
+    method: 'POST',
+    auth: {
+      'bearer': token_data.access_token,
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    qs: request_data,
+    json: false
+  }, function (err, res) {
+    if (err) {
+      //console.error(err);
+      return;
+    }
+    var data = JSON.parse(res.body);
+    console.log("BODY", JSON.stringify(data));
+    if (typeof data.error !== "undefined") {
+      console.error("YOUTUBE", "REFRESH", data.error);
+      return; // TODO: Token löschen?!?
+    }
+    token_data.access_token = data.access_token;
+    org_function(token_data);
+  });
+}
+
 function GetChannel(token_data) {
   request({
     url: 'https://www.googleapis.com/youtube/v3/channels',
@@ -87,9 +119,16 @@ function GetChannel(token_data) {
     }
   }, function (err, res) {
     if (err) {
-      console.error(err);
+      //console.error(err);
       return;
     }
-    console.log(res.body);
+    var data = JSON.parse(res.body);
+    console.log("BODY", JSON.stringify(data));
+    if (typeof data.error !== "undefined") {
+      if (data.error.code == 401) {
+        console.error("YOUTUBE", data.error.message);
+        Refresh_Token(token_data, GetChannel);
+      }
+    }
   });
 };
