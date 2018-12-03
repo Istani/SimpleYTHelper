96e9529970d4701a0e1b04a6398d75759f09e5ea
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2018-12-03 15:45:48 +0100

    TRY Envirment for Dev DB

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,28 @@
 var config = require('dotenv').config();
 // Update with your config settings.
 
-module.exports = {
-  client: 'mysql',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-  },
-  migrations: {
-    tableName: 'migrations'
-  }
-};
 
+if (process.env.ENVIROMENT == "DEV") {
+  module.exports = {
+    client: 'sqlite3',
+    connection: {
+      filename: "./mydb.sqlite"
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  };
+} else {
+  module.exports = {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  };   
+}
