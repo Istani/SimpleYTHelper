548bf8e02deb1fc6a252666b029117c83beaffdc
Sascha Kaufmann <skaufmann@ventronic.com>
2019-04-10 11:37:56 +0200

    Humble Windows

--- humble/app.js
+++ humble/app.js
@@ -107,8 +107,8 @@ async function import_data() {
         } else {
           await GameLinks.query().patch(game_data).where('name', game_data.name).where('store', game_data.store);
         }
-	console.log('Import: ',game_data.name);
-	await sleep(1000);
+        console.log('Import: ', game_data.name);
+        await sleep(1000);
       }
       fs.unlinkSync('tmp/import.json');
       console.log("===============");
@@ -122,5 +122,9 @@ async function import_data() {
   }
 }
 
-setTimeout(() => { main(); }, 12 * 60 * 60 * 1000);	// 12*1 Stunde warten bevor Start
-//main();
+var isWin = process.platform === "win32";
+if (isWin) {
+  main();
+} else {
+  setTimeout(() => { main(); }, 12 * 60 * 60 * 1000);	// 12*1 Stunde warten bevor Start
+}
