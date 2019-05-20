e9fb21652eb110d659f2f9a0265074e9b03050db
Sascha Kaufmann <skaufmann@ventronic.com>
2019-05-20 13:14:48 +0200

    Adding Twitter Pics Text

--- twitter/app.js
+++ twitter/app.js
@@ -1,25 +1,24 @@
 process.chdir(__dirname);
 const package_info = require('./package.json');
-var software=package_info.name+" (V "+package_info.version+")";
+var software = package_info.name + " (V " + package_info.version + ")";
 console.log(software);
 console.log("===");
 console.log();
-const config = require('dotenv').config({path: '../.env'});
+const config = require('dotenv').config({ path: '../.env' });
 
 var Twitter = require('twitter');
 
 const Tweets = require("./models/send_tweets.js");
 
 // https://www.npmjs.com/package/twitter
-var client = new Twitter({
+
+var client = new Twitter({  // Istani
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-// Auth as Istani! - Think about it when we add it to SYTH
-
-var client_gos = new Twitter({
+var client_gos = new Twitter({  // Games on Sale
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.GOS_TWITTER_TOKEN,
@@ -27,26 +26,64 @@ var client_gos = new Twitter({
 });
 
 async function tweet_main() {
-  client.post('statuses/update', { status: 'Testing my new App!' }, function (error, tweet, response) {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log("Tweet OK!");
-  });
+  var pre_tweet = await Tweets.query().where('user', 'Istani');
+  if (pre_tweet.length > 0) {
+    await client.post('statuses/update', { status: pre_tweet[0].message }, async function (error, tweet, response) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      await Tweets.query().delete().where('id', pre_tweet[0].id);
+    });
+  }
+  setTimeout(tweet_main, 5 * 60 * 1000);
 }
 
 async function tweet_gos() {
-  var pre_tweet = await Tweets.query().where('user','GamesOnSaleDE');
-  if (pre_tweet.length>0) {
+  var pre_tweet = await Tweets.query().where('user', 'GamesOnSaleDE');
+  if (pre_tweet.length > 0) {
     await client_gos.post('statuses/update', { status: pre_tweet[0].message }, async function (error, tweet, response) {
       if (error) {
         console.error(error);
         return;
       }
-      await Tweets.query().delete().where('id',pre_tweet[0].id);
+      await Tweets.query().delete().where('id', pre_tweet[0].id);
     });
   }
-  setTimeout(tweet_gos,5*60*1000);
+  setTimeout(tweet_gos, 5 * 60 * 1000);
 }
-tweet_gos();
+
+async function bg_gos() {
+  var img = await Jimp.read('../gamesite/public/img/games/background.png');
+  img.getBase64(Jimp.AUTO, (err, res) => {
+    if (err) {
+      return;
+    }
+    client_gos.post('account/update_profile_banner', { image: res }, async function (error, tweet, response) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+    });
+  });
+}
+async function pp_gos() {
+  var img = await Jimp.read('../gamesite/public/img/games/text.png');
+  img.getBase64(Jimp.AUTO, (err, res) => {
+    if (err) {
+      return;
+    }
+    client_gos.post('account/update_profile_image', { image: res }, async function (error, tweet, response) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+    });
+  });
+
+}
+
+bg_gos();
+pp_gos();
+//tweet_gos();
+//tweet_main();
