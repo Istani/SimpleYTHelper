f6869084a0cada88e2cf600c4c56b56b0bb120e9
Sascha Kaufmann <skaufmann@ventronic.com>
2019-07-01 13:14:21 +0200

    dont know

--- migrations/20190701123625_pk_change_ (11).js
+++ migrations/20190701123625_pk_change_ (11).js
@@ -1,7 +1,7 @@
 
 exports.up = function (knex, Promise) {
   return knex.schema.alterTable('game_check', function (t) {
-    t.string('category', 50).notNull().alter();
+    t.string('category', 20).notNull().alter();
     t.string('game', 200).notNull().alter();
   });
 };
