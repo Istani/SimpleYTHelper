4cbdfd96d6560036a421378f077203d5c8d5e4c4
Sascha Kaufmann <skaufmann@ventronic.com>
2019-08-17 12:08:22 +0200

    Package New for Main

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 process.chdir(__dirname);
 const package_info = require('./package.json');
-var software = package_info.name + " (V " + package_info.version+')';
+var software = package_info.name + " (V " + package_info.version + ')';
 console.log(software);
 console.log("===");
 console.log();
@@ -11,7 +11,7 @@ const async = require('async');
 const exec = require('child_process').execSync;
 
 /* Checking Example File for New Data! */
-var config = require('dotenv').config({path: '.env'});
+var config = require('dotenv').config({ path: '.env' });
 var config_example = "";
 if (fs.existsSync(".env")) {
   for (var attributename in config.parsed) {
@@ -24,42 +24,42 @@ if (fs.existsSync(".env")) {
   process.exit(1);
 }
 
-var need_install=false;
+var need_install = false;
 async function install() {
-  fs. readdir(__dirname, function(err, items) {
-    for (var i = 0; i<items.length;i++) {
+  fs.readdir(__dirname, function (err, items) {
+    for (var i = 0; i < items.length; i++) {
       var item = items[i];
-        if (fs.statSync(__dirname +'/' +item).isDirectory()) {
-          if (fs.existsSync(__dirname+'/'+item+'/package.json')) {
-          var tmp_pck = require(__dirname+'/'+item+'/package.json');
+      if (fs.statSync(__dirname + '/' + item).isDirectory()) {
+        if (fs.existsSync(__dirname + '/' + item + '/package.json')) {
+          var tmp_pck = require(__dirname + '/' + item + '/package.json');
           async.series([
-            (cb) => {check_modules(tmp_pck,cb);}
-          ], () => {});
+            (cb) => { check_modules(tmp_pck, cb); }
+          ], () => { });
         }
       }
     }
     if (need_install) {
-     exec('git add package.json');
-     exec('git add package-lock.json');
-     exec('git add .env.example');
-     exec('git commit -am "Package Update"');
-     exec('git push');
+      exec('git add package.json');
+      exec('git add package-lock.json');
+      exec('git add .env.example');
+      exec('git commit -am "Package Update"');
+      exec('git push');
 
-     exec('pm2 restart all');
+      exec('pm2 restart all');
     }
     process.exit(0);
   });
 }
 
-async function check_modules(package,cb) {
+async function check_modules(package, cb) {
   var debs = Object.keys(package.dependencies);
-  for (var i = 0; i<debs.length;i++) {
+  for (var i = 0; i < debs.length; i++) {
     try {
       var tmp_mods = require(debs[i]);
     } catch (e) {
-      console.error(debs[i]+': '+e.code);
-      need_install=true;
-      exec('npm install '+debs[i]);
+      console.error(debs[i] + ': ' + e.code);
+      need_install = true;
+      exec('npm install ' + debs[i]);
     }
   }
   cb();
