b9fdfd6a5aceb74c6750600d8f71eeb9e04a667c
Sascha Kaufmann <skaufmann@ventronic.com>
2019-09-25 12:43:43 +0200

    MetaGamerScore Achievements Tweet

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -83,6 +83,13 @@ module.exports = {
       error_file: "logs/GAME-CustomImport.err",
       out_file: "logs/GAME-CustomImport.log"
     },*/
+    {
+      name: "Istani-Achievements",
+      script: "./metagamerscore/app.js",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      error_file: "logs/Istani-Achievements.err",
+      out_file: "logs/Istani-Achievements.log"
+    },
     /*{
       name: 'SYTH-Amazon',
       script: "./amazon/app.js",
