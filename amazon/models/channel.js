b05ab5aae4d473f55e562372eaeffc39c34b3813
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2019-12-09 17:42:37 +0100

    Whatever

---
+++ amazon/models/channel.js
@@ -0,0 +1,25 @@
+const { Model } = require("objection");
+const Knex = require("knex");
+
+const knex = Knex(require("../knexfile.js"));
+
+Model.knex(knex);
+
+class Channel extends Model {
+  static get tableName() {
+    return "channel";
+  }
+  static get idColumn() {
+    return "user_id, service, channel_id";
+  }
+
+  $beforeInsert() {
+    this.$beforeUpdate();
+  }
+
+  $beforeUpdate() {
+    this.updated_at = new Date().toISOString();
+  }
+}
+
+module.exports = Channel;
