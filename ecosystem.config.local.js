5b9271eeccb8c2df5b4bdd25caff7e0023d82294
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2019-12-11 23:24:58 +0100

    Docker 2 Ecosystem

--- ecosystem.config.local.js
+++ ecosystem.config.local.js
@@ -9,113 +9,25 @@ module.exports = {
       autorestart: false
     },
     {
-      name: "SYTH-Backup",
-      script: "./cronjob/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/SYTH-Backup.err",
-      out_file: "logs/SYTH-Backup.log"
-    },
-    /*{
-      name: "SYTH-Web",
-      script: "./website/index.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/SYTH-Web.err",
-      out_file: "logs/SYTH-Web.log"
-    },*/
-    {
-      name: "SYTH-Discord",
-      script: "./discord/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      restart_delay: 1000,
-      error_file: "logs/SYTH-Discord.err",
-      out_file: "logs/SYTH-Discord.log"
-    },
-    /*{
-      name: "SYTH-YouTube",
-      script: "./youtube/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      restart_delay: 1000,
-      error_file: "logs/SYTH-YouTube.err",
-      out_file: "logs/SYTH-YouTube.log",
-    },*/
-    {
-      name: "SYTH-Twitter",
-      script: "./twitter/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      restart_delay: 1000,
-      error_file: "logs/SYTH-Twitter.err",
-      out_file: "logs/SYTH-Twitter.log"
-    },
-    {
-      name: "SYTH-Commands",
-      script: "./chatcommands/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      restart_delay: 1000,
-      error_file: "logs/SYTH-Commands.err",
-      out_file: "logs/SYTH-Commands.log"
-    },
-    {
-      name: "GAME-Web",
-      script: "./gamesite/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/GAME-Web.err",
-      out_file: "logs/GAME-Web.log"
-    },
-    {
-      name: "GAME-Sales",
-      script: "./gamecheck/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/GAME-Sales.err",
-      out_file: "logs/GAME-Sales.log"
-    },
-    /*{
-      name: "GAME-CustomImport",
-      script: "./test/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/GAME-CustomImport.err",
-      out_file: "logs/GAME-CustomImport.log"
-    },*/
-    /*{
-      name: 'SYTH-Amazon',
-      script: "./amazon/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/SYTH-Amazon.err",
-      out_file: "logs/SYTH-Amazon.log"
-    },*/
-    {
-      name: "SYTH-Steam",
-      script: "./steam/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/SYTH-Steam.err",
-      out_file: "logs/SYTH-Steam.log"
-    },
-    {
-      name: "SYTH-Humble",
-      script: "./humble/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/SYTH-Humble.err",
-      out_file: "logs/SYTH-Humble.log"
-    },
-    {
-      name: "SYTH-GOG",
-      script: "./gog/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/SYTH-GOG.err",
-      out_file: "logs/SYTH-GOG.log"
-    },
-    {
-      name: "Istani-Achievements",
-      script: "./metagamerscore/app.js",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-      error_file: "logs/Istani-Achievements.err",
-      out_file: "logs/Istani-Achievements.log"
-    } /*,
-    {
       name: "Istani-Tinder",
       script: "./tinder_automation/app.js",
       log_date_format: "YYYY-MM-DD HH:mm:ss Z",
       error_file: "logs/Istani-Tinder.err",
       out_file: "logs/Istani-Tinder.log"
-    }*/
+    },
+    {
+      name: "Istani-Bumble",
+      script: "./bumble_automation/app.js",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      error_file: "logs/Istani-Bumble.err",
+      out_file: "logs/Istani-Bumble.log"
+    },
+    {
+      name: "GoS-MySQL",
+      script: "./sync/app.js",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      error_file: "logs/GoS-MySQL.err",
+      out_file: "logs/GoS-MySQL.log"
+    }
   ]
 };
