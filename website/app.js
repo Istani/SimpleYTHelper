b03af50b9ee3de71e909fe973bcf29db753d7fb8
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-01-19 23:56:05 +0100

    RPG-Overlay

--- website/app.js
+++ website/app.js
@@ -215,41 +215,48 @@ app.get("/HUD/:channel/:category", async function(req, res, next) {
   var param_channel = req.params.channel;
   var param_category = req.params.category;
 
-  var data = await User_Channel.query()
-    .where("channel_id", param_channel)
-    .eager(param_category);
-
-  if (data.length > 0) {
-    var temp_data = {};
-    temp_data.data = [];
-    for (let index = 0; index < data.length; index++) {
-      const element = data[index];
-      for (let index = 0; index < element[param_category].length; index++) {
-        const element2 = element[param_category][index];
-        var tmp_data = {
-          name: element2.member_name,
-          since: element2.since,
-          length_month:
-            parseInt((new Date() - element2.since) / 1000 / 60 / 60 / 24 / 30) +
-            1
-        };
-        temp_data.data.push(tmp_data);
-      }
-      temp_data.data.sort((a, b) => {
-        if (a.since < b.since) {
-          return -1;
-        }
-        if (a.since > b.since) {
-          return 1;
-        }
-        return 0;
-      });
-    }
-    //console.log(temp_data);
+  if (param_category.startsWith("Rpg")) {
+    // RPG HUD
     res.render("hub_" + param_category, { data: temp_data });
     return;
-  }
+  } else {
+    // Service HUD
+    var data = await User_Channel.query()
+      .where("channel_id", param_channel)
+      .eager(param_category);
 
+    if (data.length > 0) {
+      var temp_data = {};
+      temp_data.data = [];
+      for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        for (let index = 0; index < element[param_category].length; index++) {
+          const element2 = element[param_category][index];
+          var tmp_data = {
+            name: element2.member_name,
+            since: element2.since,
+            length_month:
+              parseInt(
+                (new Date() - element2.since) / 1000 / 60 / 60 / 24 / 30
+              ) + 1
+          };
+          temp_data.data.push(tmp_data);
+        }
+        temp_data.data.sort((a, b) => {
+          if (a.since < b.since) {
+            return -1;
+          }
+          if (a.since > b.since) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      console.log(temp_data);
+      res.render("hub_" + param_category, { data: temp_data });
+      return;
+    }
+  }
   res.render("error", { data: temp_data });
 });
 
