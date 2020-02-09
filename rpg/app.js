eec2ba07e5b38baf4e87844b743df2c20226922f
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-02-09 20:25:34 +0100

    FixSpawn + Debug Time

--- rpg/app.js
+++ rpg/app.js
@@ -196,7 +196,7 @@ async function get_msg() {
     //console.log(temp_content);
     if (temp_content[0].startsWith(settings.prefix + "spawn")) {
       await genMonster(syth_user, msg_list[i]);
-      await genChar(syth_user, msg_list[i]);
+      //await genChar(syth_user, msg_list[i]);
     }
     if (temp_content[0].startsWith(settings.prefix + "attack")) {
       var hasCooldown = await check_cooldown(syth_user, msg_list[i]);
@@ -645,7 +645,7 @@ async function add_cooldown(syth_user, msg, time) {
   var dat = moment()
     .add(time, "seconds")
     .format();
-  console.log(dat);
+  //console.log(dat);
 
   chars[0].cooldown = dat;
 
